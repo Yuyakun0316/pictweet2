c7b02016f90c67c2dcb65968bc1d5e2c4f630596
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-10-28 17:00:26 +0900

    editアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create, :destroy]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
 end
