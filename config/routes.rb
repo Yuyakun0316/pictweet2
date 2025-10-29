b3c8b10d7518922406279c488d7ab56c687cb804
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-10-29 12:41:08 +0900

    createアクションのルーティングを設定
    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   root to: 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
